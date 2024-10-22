6aca278eae8ecc7d7e8e10d9e59a7518a86a2069
Smiling0022 <wilasinee200120@gmail.com>
2024-10-22 20:36:18 +0700

    Ex_code

---
+++ Water_bill.py
@@ -0,0 +1,10 @@
+# Water_bill.py
+
+# ตัวอย่างการส่งค่า
+def calculate_water_bill(water_usage, price_per_unit):
+    sum_water = water_usage * price_per_unit
+    print(f"Total water bill: {sum_water} baht")
+    return sum_water
+
+# Example usage
+#calculate_water_bill(30, 10)  # ส่งค่าการใช้น้ำเป็น 30 หน่วย และราคาต่อหน่วย 10 บาท
---
+++ calculator.py
@@ -0,0 +1,6 @@
+# calculator.py
+
+def calculate_salary_deduction(salary, total_expenses):
+    # คำนวณหักลบค่าใช้จ่ายจากเงินเดือน
+    remaining_salary = salary - total_expenses
+    return remaining_salary
